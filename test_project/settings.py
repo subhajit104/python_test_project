235e43d592f62423832beeb36af95e9fdccb6877
subhajit104 <subhajit.manna.104@gmail.com>
2018-08-01 11:27:06 +0530

    seventh times

--- test_project/settings.py
+++ test_project/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
